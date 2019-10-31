cefd97f89997deb8c953673443bc94fccffd8374
Kiran <Kiran@Dhivyas-MBP-2.fritz.box>
2019-10-31 23:19:04 +1300

    Add tests

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,150 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ASB_0_document_0_phone.png",
+        "test": "../bitmaps_test/20191031-101622/backstop_default_ASB_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_ASB_0_document_0_phone.png",
+        "label": "ASB",
+        "misMatchThreshold": 0.1,
+        "url": "https://asb.co.nz",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -38
+          },
+          "misMatchPercentage": "7.18",
+          "analysisTime": 139
+        },
+        "diffImage": "../bitmaps_test/20191031-101622/failed_diff_backstop_default_ASB_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ASB_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20191031-101622/backstop_default_ASB_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_ASB_0_document_1_tablet.png",
+        "label": "ASB",
+        "misMatchThreshold": 0.1,
+        "url": "https://asb.co.nz",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -1
+          },
+          "misMatchPercentage": "1.92",
+          "analysisTime": 193
+        },
+        "diffImage": "../bitmaps_test/20191031-101622/failed_diff_backstop_default_ASB_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ANZ_0_document_0_phone.png",
+        "test": "../bitmaps_test/20191031-101622/backstop_default_ANZ_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_ANZ_0_document_0_phone.png",
+        "label": "ANZ",
+        "misMatchThreshold": 0.1,
+        "url": "https://anz.co.nz",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -6,
+            "height": 0
+          },
+          "misMatchPercentage": "25.23",
+          "analysisTime": 110
+        },
+        "diffImage": "../bitmaps_test/20191031-101622/failed_diff_backstop_default_ANZ_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ANZ_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20191031-101622/backstop_default_ANZ_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_ANZ_0_document_1_tablet.png",
+        "label": "ANZ",
+        "misMatchThreshold": 0.1,
+        "url": "https://anz.co.nz",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -18
+          },
+          "misMatchPercentage": "24.72",
+          "analysisTime": 271
+        },
+        "diffImage": "../bitmaps_test/20191031-101622/failed_diff_backstop_default_ANZ_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_TestingMind_0_document_0_phone.png",
+        "test": "../bitmaps_test/20191031-101622/backstop_default_TestingMind_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_TestingMind_0_document_0_phone.png",
+        "label": "TestingMind",
+        "misMatchThreshold": 0.1,
+        "url": "https://www.testingmind.com/aboutus/",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -55
+          },
+          "misMatchPercentage": "14.72",
+          "analysisTime": 174
+        },
+        "diffImage": "../bitmaps_test/20191031-101622/failed_diff_backstop_default_TestingMind_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_TestingMind_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20191031-101622/backstop_default_TestingMind_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_TestingMind_0_document_1_tablet.png",
+        "label": "TestingMind",
+        "misMatchThreshold": 0.1,
+        "url": "https://www.testingmind.com/aboutus/",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "12.28",
+          "analysisTime": 247
+        },
+        "diffImage": "../bitmaps_test/20191031-101622/failed_diff_backstop_default_TestingMind_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
